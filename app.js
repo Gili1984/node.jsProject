be0600cbe358b4527ceadc56169f764381caf48e
Gili1984 <gilizilber6@gmail.com>
2023-12-04 13:51:58 +0200

    first commit

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const toysRoutes = require("./routes/toys.routes");
 const usersRoutes = require("./routes/users.routes");
+const path = require("path");
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 // { origin: ['http://127.0.0.1:5500/', 'http://127.0.0.1:5501/'] }
 app.use(cors());
